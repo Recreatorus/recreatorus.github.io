209d2f34dcb92a03109f4f331817b31b16609de7
recreatorus <recreatorus@gmail.com>
2016-07-15 13:52:47 +0300

    update all

--- js/main.js
+++ js/main.js
@@ -1,21 +1,14 @@
 $('.rcr-primary-nav').click(function () {
 	$(this).children('ul').toggleClass('is-visible');
 	});
-
-/*	jQuery(function($){
-	$(document).mouseup(function (e){ // событие клика по веб-документу
-		var div = $('.rcr-primary-nav'); // тут указываем ID элемента
-		if (!div.is(e.target) // если клик был не по нашему блоку
-		    && div.has(e.target).length === 0) { // и не по его дочерним элементам
-			$(this).children('ul').removeClass('is-visible'); // скрываем его
+	
+// Click outside of mobile menu 
+jQuery(function($){
+	$(document).mouseup(function (e){
+		var div = $('.rcr-primary-nav');
+		if (!div.is(e.target)
+		    && div.has(e.target).length === 0) {
+			$('.rcr-primary-nav').children('ul').removeClass('is-visible');
 		}
 	});
-});	*/
-	
-
-/*	$(document).mouseup(function (e) {
-    var container = $('.rcr-primary-nav');
-    if (container.has(e.target).length === 0){
-        $(this).children('ul').removeClass('is-visible');
-    }
-}); */
+});	
