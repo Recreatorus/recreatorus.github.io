5ff86e45a0d8ae5350dc61cbfa4c08b43aeb894d
recreatorus <recreatorus@gmail.com>
2016-07-15 16:24:24 +0300

    css

--- js/main.js
+++ js/main.js
@@ -1,6 +1,9 @@
+// Mobile menu 
 $('.rcr-primary-nav').click(function () {
 	$(this).children('ul').toggleClass('is-visible');
-	});
+	$(this).toggleClass('open-mobile');
+});
+
 	
 // Click outside of mobile menu 
 jQuery(function($){
@@ -9,6 +12,9 @@ jQuery(function($){
 		if (!div.is(e.target)
 		    && div.has(e.target).length === 0) {
 			$('.rcr-primary-nav').children('ul').removeClass('is-visible');
+			$('.rcr-primary-nav').removeClass('open-mobile');
 		}
 	});
-});	
+});
+
+// mobile menu animation
