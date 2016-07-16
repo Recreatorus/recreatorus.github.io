0f46ac1ac39a84f3af36060747d4f4582b252c53
recreatorus <recreatorus@gmail.com>
2016-07-16 13:34:57 +0300

    gallery slideshow

--- gallery/js/main.js
+++ gallery/js/main.js
@@ -160,7 +160,18 @@
 						}
 
 					});
+		
+		// slideshow
+			var slides = document.querySelectorAll('#slides .slide');
+			var currentSlide = 0;
+			var slideInterval = setInterval(nextSlide,6000);
 
+			function nextSlide(){
+				slides[currentSlide].className = 'slide';
+				currentSlide = (currentSlide+1)%slides.length;
+				slides[currentSlide].className = 'slide showing';
+			}
+			
 		// Header.
 			var $header = $('#header');
 
--- js/main.js
+++ js/main.js
@@ -17,4 +17,4 @@ jQuery(function($){
 	});
 });
 
-// mobile menu animation
+
